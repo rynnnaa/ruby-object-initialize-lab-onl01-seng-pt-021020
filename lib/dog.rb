e6ebb5f9d9ad4680f969dc5e5b923df3a5646bed
Ryna Truong <rynatruong@outlook.com>
2020-02-26 03:57:33 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def initialize(:name, :breed)
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+end
